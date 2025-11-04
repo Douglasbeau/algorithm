2b2a5fd276b2af7d70cd22826c5474a9f3c73010
Douglasbeau <2821609983@qq.com>
2025-11-04 21:51:18 +0800

    search matrix - add testing to test the performance of 2 approaches

--- src/matrix/SearchMatrix2.java
+++ src/matrix/SearchMatrix2.java
@@ -8,8 +8,7 @@ public class SearchMatrix2 {
         int col = matrix[0].length;
         if (row == 1 && col == 1)
             return matrix[0][0] == target;
-        // 法一、二分
-        // return search(matrix, 0, row-1, 0, col-1, target);
+
         // 法二、z字形
         for (int i=0, j=col-1; i<row && j>=0;) {
             if (target == matrix[i][j])
@@ -21,6 +20,13 @@ public class SearchMatrix2 {
         }
         return false;
     }
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        // 法一、二分
+         return search(matrix, 0, row-1, 0, col-1, target);
+    }
+
     // 需要行的起、止位置，列的起、止位置
     boolean search(int[][] matrix, int rs, int re, int cs, int ce, int target) {
         if(rs > re || cs > ce)
@@ -49,5 +55,37 @@ public class SearchMatrix2 {
         }
         return search(matrix, rs, mr-1, l+1, ce, target) ||
                 search(matrix, mr+1, re, cs, l, target);
+    }
+
+    public static void main(String[] args) {
+        int N = 8000;
+        int[][] matrix = new int[N][N];
+        int v = 1;
+        // 斜线填充，确保下面大，右面大
+        for (int i = 0; i < N; i++) {
+            for (int j = i, k = 0; j >= 0; j--, k++) {
+                matrix[j][k] = v++;
+            }
+        }
+        // 到底了 起点总在(3999, k++)
+        for (int i = 0; i < N-1; i++) {
+            for (int j = N-1, k=i+1; k < N; k++, j--) {
+                matrix[j][k] = v++;
+            }
+        }
+        System.out.println(matrix[300][300]);
+        System.out.println(matrix[3000][400]);
+        System.out.println(matrix[4000][4000]);
+        System.out.println(matrix[7999][7888]);
+        System.out.println(matrix[7999][7889]);
+        SearchMatrix2 searchMatrix2 = new SearchMatrix2();
+
+        long start = System.nanoTime();
+        boolean b = searchMatrix2.searchMatrix2(matrix, 200_000_000);
+        System.out.println(b + " m2 time cost, " + (System.nanoTime() - start));
+
+        start = System.nanoTime();
+        b = searchMatrix2.searchMatrix(matrix, 200_000_000);
+        System.out.println(b + " m1 time cost: " + (System.nanoTime()-start));
     }
 }
