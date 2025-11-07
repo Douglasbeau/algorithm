eff9baad55086ddef9e61fe88536eb8eabced8eb
Douglasbeau <2821609983@qq.com>
2025-11-07 19:27:40 +0800

    enhanced heap to support value update (for unique values

--- src/array/Heap.java
+++ src/array/Heap.java
@@ -1,18 +1,26 @@
 package array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-// 堆的实现
+// 堆的实现，加上值更新操作，同时维持堆结构
 // 堆排序
 public class Heap {
     private final int[] nums;
     private int heapSize;
+    private Map<Integer, Integer> numPos;
     public Heap(int capacity) {
         nums = new int[capacity];
+        numPos = new HashMap<>();
     }
     public Heap(int[] nums) { //用于一次给出整个数组的情况
         this.nums = nums;
         heapSize = nums.length;
+        numPos = new HashMap<>();
+        for (int i=0; i< nums.length; i++) {
+            numPos.put(nums[i], i);
+        }
         int i = nums.length - 1;
         while (i >= 0) {
             heapify(i);
@@ -20,39 +28,61 @@ public class Heap {
         }
     }
 
-    // 插入末尾，往上调整
-    public void heapInsert(int num) {
-        nums[heapSize++] = num;
+    public void add(int val) {
+        numPos.put(val, heapSize);
+        nums[heapSize++] = val;
+        heapInsert(heapSize - 1);
+    }
 
-        int index = heapSize - 1;
-        while(nums[index] > nums[(index - 1)/2]) {
-            swap(index, (index - 1)/2);
-            index = (index-1)/2;
+    // 插入末尾，往上调整。记录num->index
+    public void heapInsert(int idx) {
+        int p;
+        while((p = idx - 1 >> 1) >= 0 && nums[idx] > nums[p]) {
+            swap(idx, p);
+            numPos.put(nums[idx], idx);
+            idx = p;
+            numPos.put(nums[idx], idx);
         }
+    }
+    public void update(int v, int nv) {
+        if (v == nv)
+            return;
+        int pos = numPos.getOrDefault(v, -1);
+        if (pos == -1)
+            return;
+        nums[pos] = nv;
+        numPos.put(nv, pos);
+        numPos.remove(v);
+        // 下面只会中一个分支
+        heapify(pos);
+        heapInsert(pos);
     }
 
     public int poll() {
         int head = nums[0];
+        numPos.remove(head);
         swap(0, --heapSize);
         heapify(0);
         return head;
     }
 
-    // 下沉
+    // 小的下沉
     void heapify(int i) {
-        while(2*i + 1 < heapSize) {
-            int left = i*2 + 1;
-            int right = left + 1;
+        int left;
+        while((left = i << 1 | 1) < heapSize) {
             int big;
-            if (right < heapSize) {
-                big = nums[left] > nums[right] ? left : right;
+            if (left + 1 < heapSize) {
+                big = nums[left] > nums[left + 1] ? left : left + 1;
             } else {
                 big = left;
             }
+            // nums[i] big enough, no more need to sink
             if (nums[big] <= nums[i])
                 break;
             swap(i, big);
+            numPos.put(nums[i], i);
             i = big;
+            numPos.put(nums[i], i);
         }
     }
 
@@ -64,6 +94,16 @@ public class Heap {
         }
     }
 
+    // remove head to tail, and heapify
+    public int[] sorted() {
+        while(heapSize > 0) {
+            swap(0, --heapSize);
+            // root could be small
+            heapify(0);
+        }
+        return nums;
+    }
+
     public void print() {
         int i = 0;
         while(i < heapSize - 1){
@@ -72,29 +112,34 @@ public class Heap {
         System.out.println(nums[i]);
     }
 
-    public int[] getNums() {
-        return nums;
-    }
-
-
     public static void main(String[] args) {
-        final Heap heap = new Heap(6);
+        final Heap ih = new Heap(9);
         // 逐个插入
-        heap.heapInsert(2);
-        heap.heapInsert(3);
-        heap.heapInsert(4);
-        heap.heapInsert(1);
-        heap.heapInsert(5);
-        heap.heapInsert(7);
-        heap.print();
+        ih.add(2);
+        ih.add(3);
+        ih.add(4);
+        ih.add(1);
+        ih.add(5);
+        ih.add(7);
+        ih.add(9);
+        ih.add(10);
+        ih.add(11);
+        System.out.println("before update");
+        ih.print();
+        ih.update(5, 8);
+        System.out.println("after update");
+        ih.print();
+        System.out.println("sorted:");
+        System.out.println(Arrays.toString(ih.sorted()));
+        System.out.println("---");
 
         // 堆排序，1. 从下往上遍历heapify下沉，2.弹出并进行heapify
-        int[] nums = new int[] {1, 2, 3, 5, 6, 8};
-        final Heap sort = new Heap(nums);
-        sort.print();
-        while (sort.heapSize > 0) {
-            System.out.println(sort.poll());;
-        }
-        System.out.println(Arrays.toString(sort.nums));
+        int[] nums = new int[] {1, 2, 9, 7, 10, 20, 21, 11, 5, 6, 8};
+        final Heap sh = new Heap(nums);
+        sh.print();
+        sh.update(8, 13);
+        sh.print();
+        System.out.println(Arrays.toString(sh.sorted()));
+        System.out.println(Arrays.toString(sh.nums));
     }
 }
--- src/array/HeapSort.java
+++ src/array/HeapSort.java
@@ -1,15 +1,23 @@
 package array;
 
+import java.util.Arrays;
+
 // heap sort with all numbers provided at first
 public class HeapSort {
+    public static void main(String[] args) {
+        HeapSort heapSort = new HeapSort();
+        int[] a = new int[]{11, 21, 30, 4, 15, 6, 7, 8, 9};
+        heapSort.sortArray(a);
+        System.out.println(Arrays.toString(a));
+    }
     public int[] sortArray(int[] nums) {
         // heap sort
         int n = nums.length;
         // big root, by heapify
         for (int i=n-1; i>=0; i--) {
-            heapify(nums, n - 1, i);
+            heapify(nums, n-1, i);
         }
-        int tail = n - 1;
+        int tail = nums.length - 1;
         // del root n times, moved to tail
         for (int i=0; i<n; i++) {
             swap(nums, 0, tail);
@@ -18,17 +26,16 @@ public class HeapSort {
         return nums;
     }
 
+    // 小值往下沉
     void heapify(int[] nums, int lastIdx, int i) {
         while (2 * i + 1 <= lastIdx) {
             int l = i << 1 | 1;
             int r = l + 1;
             int big = r > lastIdx || nums[l] >= nums[r] ? l : r;
-            if (nums[big] > nums[i]) {
-                swap(nums, i, big);
-                i = big;
-            } else {
+            if (nums[big] <= nums[i])
                 break;
-            }
+            swap(nums, i, big);
+            i = big;
         }
     }
 
