85ea306182308ea57969d69e7d40bc56a1bb4a12
Douglasbeau <2821609983@qq.com>
2025-11-07 20:05:15 +0800

    construct balanced BST from sorted list

---
+++ src/tree/ConstructBST.java
@@ -0,0 +1,35 @@
+package tree;
+
+import common.ListNode;
+
+// LC 109 有序链表构建平衡二叉搜索树，中点+递归
+public class ConstructBST {
+    public TreeNode sortedListToBST(ListNode head) {
+        return construct(head, null);
+    }
+
+    // end is excluded
+    TreeNode construct(ListNode head, ListNode end) {
+        if (head == end)
+            return null;
+        ListNode mid = middle(head, end);
+        TreeNode th = new TreeNode(mid.val);
+        // link l,r
+        TreeNode left = construct(head, mid);
+        TreeNode right = construct(mid.next, end);
+        th.left = left;
+        th.right = right;
+        return th;
+    }
+
+    // right middle
+    ListNode middle(ListNode node, ListNode end) {
+        ListNode slow = node;
+        ListNode fast = node;
+        while (fast != end && fast.next != end) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
